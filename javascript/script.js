99a63126723a2328cafdeadf42232871bcfffec1
madelynm3 <79669704+madelynm3@users.noreply.github.com>
2022-01-22 20:36:33 -0700

    update site plan

---
+++ javascript/script.js
@@ -0,0 +1,6 @@
+let lastModified = new Date(document.lastModifed)
+let today = new Date()
+
+document.getElementById("year").innerText = today.getFullYear()
+document.getElementById("date").innerText = lastModified.toLocaleDateString()
+document.getElementById("time").innerText = lastModified.toLocaleString()
