bba9e9236a8f1326d90254097ce12910565b7a15
madelynm3 <79669704+madelynm3@users.noreply.github.com>
2022-01-28 14:34:04 -0700

    update

--- javascript/script.js
+++ javascript/script.js
@@ -1,8 +1,7 @@
 let lastModified = new Date(document.lastModifed)
 let today = new Date()
 
-function getElementById() {
+
     document.getElementById("year").innerText = today.getFullYear()
     document.getElementById("date").innerText = lastModified.toLocaleDateString()
     document.getElementById("time").innerText = lastModified.toLocaleString()
-}
