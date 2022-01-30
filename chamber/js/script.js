05cb02b2166a51940808636d7cf4c40da83ef54d
Maddy McGreer <79669704+madelynm3@users.noreply.github.com>
2022-01-30 15:25:13 -0700

    Update script.js

--- chamber/js/script.js
+++ chamber/js/script.js
@@ -1,7 +1,8 @@
-
+// select the elements to manipulate (output to)
 const datefield = document.querySelector("date");
-const datefieldUK = document.querySelector("aside");
+const datefieldUK = document.querySelector("aside"); // for european/family history format with day first.
 
+// derive the current date using a date object
 const now = new Date();
 const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
 	now
@@ -9,7 +10,7 @@ const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
 const fulldateUK = new Intl.DateTimeFormat("en-UK", {
 	dateStyle: "full"
 }).format(now);
-
+// long, medium, short options ... try them
 
 datefield.innerHTML = `<em>${fulldate}</em>`;
 datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
