8d97b171b477646069a28104d07a0d97e437120e
madelynm3 <79669704+madelynm3@users.noreply.github.com>
2022-01-29 17:41:06 -0700

    adding css and js

---
+++ chamber/js/script.js
@@ -0,0 +1,15 @@
+
+const datefield = document.querySelector("date");
+const datefieldUK = document.querySelector("aside");
+
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+	now
+);
+const fulldateUK = new Intl.DateTimeFormat("en-UK", {
+	dateStyle: "full"
+}).format(now);
+
+
+datefield.innerHTML = `<em>${fulldate}</em>`;
+datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
