90a91fe1ec0a0d8485ee0bd98658524657961dc8
madelynm3 <79669704+madelynm3@users.noreply.github.com>
2022-03-04 22:42:00 -0700

    u

--- lesson9/prophets.js
+++ lesson9/prophets.js
@@ -1,4 +1,6 @@
-const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
+const requestURL = "https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json";
+
+
 
 fetch(requestURL)
   .then(function (response) {
@@ -15,19 +17,21 @@ prophets.forEach(displayProphets);
   function displayProphets(prophet) {
     // Create elements to add to the document
     let card = document.createElement('section');
+    
     let h2 = document.createElement('h2');
+    h2.textContent = ('${prophet.name + prophet.lastname}'); 
 
-    let dob = document.createElement('prophet');
-    dob.textContent = ("Date of Birth: ${prophet.birthdate}");
+    let dob = document.createElement('p');
+    dob.textContent = (`Date of Birth: ${prophet.birthdate}`);
 
-    let birthPlace = document.createElement('prophet');
-    birthPlace.textContent = ("Place of Birth: ${prophet.birthdate}");
+    let birthPlace = document.createElement('p');
+    birthPlace.textContent = (`Place of Birth: ${prophet.birthdate}`);
 
     let portrait = document.createElement('img');
     portrait.src = prophet.imageurl;
     portrait.alt = 'image of prophet';
 
-    h2.textContent = ("${prophet.name + prophet.lastname}"); 
+    
 
     card.appendChild(prophetName);
     card.appendChild(dob);
