b8af69ced2ed817584f4ece2fcb595dd46abcafb
madelynm3 <79669704+madelynm3@users.noreply.github.com>
2022-03-04 22:29:37 -0700

    u

--- lesson9/prophets.js
+++ lesson9/prophets.js
@@ -1,7 +1,5 @@
 const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
 
-
-
 fetch(requestURL)
   .then(function (response) {
     return response.json();
