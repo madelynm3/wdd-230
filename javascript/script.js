7856925577bcc848c18fdaffa247a1050eeaf8bf
madelynm3 <79669704+madelynm3@users.noreply.github.com>
2022-03-04 21:17:17 -0700

    update portal

--- javascript/script.js
+++ javascript/script.js
@@ -1,7 +1,7 @@
-let lastModified = new Date(document.lastModifed)
+let lastModified = new Date(document.lastModifed);
 let today = new Date()
 
 
-    document.getElementById("year").innerText = today.getFullYear()
-    document.getElementById("date").innerText = lastModified.toLocaleDateString()
-    document.getElementById("time").innerText = lastModified.toLocaleString()
+document.getElementById("year").innerText = today.getFullYear();
+document.getElementById("date").innerText = lastModified.toLocaleDateString();
+document.getElementById("time").innerText = lastModified.toLocaleString();
