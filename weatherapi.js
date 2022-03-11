119cf829906e01fb2d692e8baad6e5e20ac3ff1d
Maddy McGreer <79669704+madelynm3@users.noreply.github.com>
2022-03-10 21:26:17 -0700

    Create weatherapi.js

---
+++ weatherapi.js
@@ -0,0 +1,14 @@
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?q=Fairbanks&units=imperial&appid=27e20167da1b1c1b0c28e5fae1b1f633";
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+  document.querySelector('#current-temp').textContent = jsObject.main.temp;
+  });
+
+const iconsrc= `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
+const desc = jsObject.weather[0].description;
+document.querySelector('#icon-src').textContent = iconsrc;
+document.querySelector('#weathericon').setAttribute('src', iconsrc);
+document.querySelector('#weathericon').setAttribute('alt', desc);
+document.querySelector('figcaption').textContent = desc;
