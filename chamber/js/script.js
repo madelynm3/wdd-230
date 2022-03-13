7be496e2951de8e96ccfae7d892886c63b228625
madelynm3 <79669704+madelynm3@users.noreply.github.com>
2022-03-12 20:34:56 -0700

    update

--- chamber/js/script.js
+++ chamber/js/script.js
@@ -24,24 +24,24 @@ fetch(apiURL)
   document.querySelector('#current-temp').textContent = jsObject.main.temp;
   document.querySelector('#wind-speed').textContent = jsObject.wind.speed;
   document.querySelector('#description').textContent = jsObject.weather[0].description;
-  
+  const iconsrc= `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
+  const desc = jsObject.weather[0].description;
 
-const iconsrc= `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
-const desc = jsObject.weather[0].description;
-document.querySelector('#weathericon').setAttribute('src', iconsrc);
-document.querySelector('#weathericon').setAttribute('alt', desc);
-document.querySelector('figcaption').textContent = desc;
+  document.querySelector('#weathericon').setAttribute('src', iconsrc);
+  document.querySelector('#weathericon').setAttribute('alt', desc);
+  document.querySelector('figcaption').textContent = desc;
 
-const temp = Number(document.getElementById("current-temp").textContent);
-const speed = Number(document.getElementById("wind-speed").textContent);
-document.getElementById("wind-chill").textContent = chill(temp, speed);
-function chill(temp, speed) {
-  if ((temp <= 50) & (speed >= 3)) {
-    return Math.round(
-      35.74 + 0.6215 * temp - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16)
-    );
-  } else {
-    return "N/A";
-  }
-}
-});
+  const temp = Number(document.getElementById("current-temp").textContent);
+  const speed = Number(document.getElementById("wind-speed").textContent);
+  document.getElementById("wind-chill").textContent = chill(temp, speed);
+  function chill(temp, speed) {
+    if ((temp <= 50) & (speed >= 3)) {
+      return Math.round(
+        35.74 + 0.6215 * temp - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16)
+        );
+      }
+      else {
+        return "N/A";
+      }
+    }
+  });
