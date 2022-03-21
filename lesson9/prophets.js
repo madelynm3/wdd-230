ff7ca5c9751fd485c2362a935419a15ab71a1260
madelynm3 <79669704+madelynm3@users.noreply.github.com>
2022-03-21 15:08:29 -0600

    formatting

--- lesson9/prophets.js
+++ lesson9/prophets.js
@@ -1,46 +1,39 @@
-const requestURL = "https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json";
-
-
+const requestURL =
+  "https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json";
 
 fetch(requestURL)
   .then(function (response) {
     return response.json();
   })
   .then(function (jsonObject) {
-    console.table(jsonObject);  // temporary checking for valid response and data parsing
+    console.table(jsonObject); // temporary checking for valid response and data parsing
   });
 
-const prophets = jsonObject['prophets'];
+const prophets = jsonObject["prophets"];
 
 prophets.forEach(displayProphets);
 
-  function displayProphets(prophet) {
-    // Create elements to add to the document
-    let card = document.createElement('section');
-    
-    let h2 = document.createElement('h2');
-    h2.textContent = prophet.name + ' ' + prophet.lastname;
+function displayProphets(prophet) {
+  // Create elements to add to the document
+  let card = document.createElement("section");
 
+  let h2 = document.createElement("h2");
+  h2.textContent = prophet.name + " " + prophet.lastname;
 
-    let dob = document.createElement('p');
-    dob.textContent = `Date of Birth: ${prophet.birthdate}`
+  let dob = document.createElement("p");
+  dob.textContent = `Date of Birth: ${prophet.birthdate}`;
 
-    let birthPlace = document.createElement('p');
-    birthPlace.textContent = `Place of Birth: ${prophet.birthdate}`
+  let birthPlace = document.createElement("p");
+  birthPlace.textContent = `Place of Birth: ${prophet.birthdate}`;
 
-    let pic = document.createElement('img');
-    pic.src = prophet.imageurl;
-    pic.alt = 'image of prophet';
+  let pic = document.createElement("img");
+  pic.src = prophet.imageurl;
+  pic.alt = "image of prophet";
 
-    
+  card.appendChild(h2);
+  card.appendChild(dob);
+  card.appendChild(birthPlace);
+  card.appendChild(pic);
 
-    card.appendChild(h2);
-    card.appendChild(dob);
-    card.appendChild(birthPlace);
-    card.appendChild(pic);
-
-    document.querySelector('div.cards').appendChild(card);
-  }
-
-  
-  
+  document.querySelector("div.cards").appendChild(card);
+}
