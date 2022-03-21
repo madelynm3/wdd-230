22bb8810f0a376299503a61520b97477f56a9986
madelynm3 <79669704+madelynm3@users.noreply.github.com>
2022-03-21 14:43:07 -0600

    formatting

--- lesson7/script.js
+++ lesson7/script.js
@@ -1,36 +1,31 @@
-
 const images = document.querySelectorAll("[data-src]");
 
 function preloadImage(img) {
-const src = image.getAttribute("data-src");
+  const src = image.getAttribute("data-src");
 
-if(!src) {
-return; }
+  if (!src) {
+    return;
+  }
 
-img.src = src;
-
+  img.src = src;
 }
 
-const imgOptions = { 
-threshold: 1,
-rootMargin: "0px 0px 300px 0px"
-
+const imgOptions = {
+  threshold: 1,
+  rootMargin: "0px 0px 300px 0px",
 };
 
-const imgObserver = new IntersectionObserver((entries, imgObserver) => 
-{ 
-entries.ForEach(entry => { 
+const imgObserver = new IntersectionObserver((entries, imgObserver) => {
+  entries.ForEach((entry) => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      preloadImage(entry.target);
+      imgObserver.unobserve(entry.target);
+    }
+  });
+}, imgOptions);
 
-if (!entry.isIntersecting) { 
-return
-   } else { preloadImage(entry.target); 
-   imgObserver.unobserve(entry.target); }
-   
-   }) 
-   
-   }, imgOptions);
-   
-   images.forEach(image => { 
-   imgObserver.observe(image);
-   
-   })
+images.forEach((image) => {
+  imgObserver.observe(image);
+});
